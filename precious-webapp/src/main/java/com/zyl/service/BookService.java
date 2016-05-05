492f7242526331503f0e4c74126292efad1d8ee6
Pirate-Captain <1531515382@qq.com>
2016-05-05 13:29:09 +0800

    Modify pom info

--- precious-webapp/src/main/java/com/zyl/service/BookService.java
+++ precious-webapp/src/main/java/com/zyl/service/BookService.java
@@ -5,10 +5,10 @@ package com.zyl.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 
-import com.chsi.framework.util.RandomUtil;
 import com.zyl.book.model.BookModel;
 
 /**
@@ -17,10 +17,9 @@ import com.zyl.book.model.BookModel;
  */
 public class BookService {
     private static List<BookModel> bookModelList = new ArrayList<BookModel>();
-    
     static {
         BookModel model1 = new BookModel();
-        model1.setId(RandomUtil.getRandomString(16));
+        model1.setId(StringUtils.replace(UUID.randomUUID().toString(),"-",""));
         model1.setAuthor("张三");
         model1.setName("相对论的因式分解");
         model1.setPrice(188.8f);
---
+++ precious-webapp/src/main/java/com/zyl/zookeeper/ZookeeperBaseApi.java
@@ -0,0 +1,51 @@
+/**
+ * Created on 2016-5-4
+ */
+package com.zyl.zookeeper;
+
+import java.io.IOException;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.junit.Test;
+
+/**
+ * @author zhuyl<a href="mailto:zhuyl@chsi.com.cn">zhu Youliang</a>
+ * @version $Id$
+ */
+public class ZookeeperBaseApi {
+    public static void main(String[] args) throws IOException {
+        new ZookeeperBaseApi().testT();
+    }
+    
+    @Test
+    public void testT() throws IOException {
+        ZooKeeper zk = new ZooKeeper("192.168.40.131:2181", 10000, new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                System.out.println(event.getType());
+            }
+        });
+        
+        try {
+            String createParentDirResult = zk.create("/zk_learn", "Parent directory".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            System.out.println(createParentDirResult);
+            String createResult = zk.create("/zk_learn/first", "Hello world".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+            System.out.println(createResult);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                zk.close();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
