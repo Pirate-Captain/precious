e2f549098554134a4210a88edaef51bd38567355
Pirate-Captain <1531515382@qq.com>
2016-06-15 11:33:02 +0800

    Add kafka test
    kafka test

---
+++ precious-webapp/src/main/java/com/zyl/kafka/producer/KafkaProducerTest.java
@@ -0,0 +1,14 @@
+/**
+ * Created on 2016-6-8
+ */
+package com.zyl.kafka.producer;
+
+/**
+ * @author zhuyl<a href="mailto:zhuyl@chsi.com.cn">zhu Youliang</a>
+ * @version $Id$
+ */
+public class KafkaProducerTest {
+    public static void main(String[] args) {
+        
+    }
+}
