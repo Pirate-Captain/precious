d98e51f7e11f7e626fa870e31571d4ae84fc5c2c
Pirate-Captain <1531515382@qq.com>
2016-06-15 17:26:26 +0800

    First Hadoop File Read example

---
+++ precious/src/main/java/com/zyl/hadoop/file/HadoopUrlReadFile.java
@@ -0,0 +1,25 @@
+package com.zyl.hadoop.file;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.io.IOUtils;
+
+public class HadoopUrlReadFile {
+    static {
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+    public static void main(String[] args) {
+        InputStream ins = null;
+        try {
+            ins = new URL("hdfs://node:9000/input.txt").openStream();
+            IOUtils.copyBytes(ins, System.out, 1024, false);
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeStream(ins);
+        }
+    }
+}
