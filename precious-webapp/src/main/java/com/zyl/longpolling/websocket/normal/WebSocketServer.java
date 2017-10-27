8af4d0e77779c2a18b81da8f7591ad0d9151db06
Pirate-Captain <1531515382@qq.com>
2017-10-27 15:08:06 +0800

    Websocket used just websocket api

---
+++ precious-webapp/src/main/java/com/zyl/longpolling/websocket/normal/WebSocketServer.java
@@ -0,0 +1,57 @@
+/**
+ * chsi
+ * Created on 2017-10-27
+ */
+package com.zyl.longpolling.websocket.normal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
+/**
+ * @author zhuyl<a href="mailto:zhuyl@chsi.com.cn">zhu Youliang</a>
+ * @version $Id$
+ */
+@ServerEndpoint("/websocket/{user}")
+public class WebSocketServer {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+    private String currentUser;
+
+    @OnOpen
+    public void onOpen(@PathParam("user") String user, Session session) {
+        this.currentUser = user;
+        log.info("websocket-connect-登录的用户：{}", currentUser);
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        log.info("websocket-onMessage-user:{} say: {}", currentUser, message);
+        //异步发送消息
+//        session.getAsyncRemote().sendText(currentUser + " say：" + message);
+        try {
+            //同步发送消息
+            session.getBasicRemote().sendText(currentUser + " say: " + message);
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnClose
+    public void onClose(Session session, CloseReason closeReason) {
+        log.info("websocket-onclose-user:{},sessionid:{} close by {}", currentUser, session.getId(), closeReason);
+    }
+
+    @OnError
+    public void onError(Throwable exception) {
+        log.info("websocket-onerror-exception:{}", exception);
+    }
+}
---
+++ precious-webapp/src/main/webapp/asyncWebsocketNormal.jsp
@@ -0,0 +1,44 @@
+<%@ page contentType="text/html; charset=UTF-8" language="java" errorPage="" %>
+<%@taglib prefix="s" uri="/struts-tags" %>
+<!DOCTYPE>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title> Async Servlet 3.0 </title>
+        <script type="text/javascript" src="/js/jquery-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td><input type="button" value="建立连接" id="open"></td>
+                <td><input type="text" id="message"></td>
+                <td><input type="button" value="发送消息" id="sendMessage"></td>
+            </tr>
+        </table>
+    </body>
+</html>
+<script type="text/javascript">
+    var websocket = null;
+    $(function () {
+        $("#open").click(function () {
+            if ( 'WebSocket' in window ) {
+                websocket = new WebSocket("ws://localhost/websocket/ws.do");
+                websocket.onopen = function (event) {
+                    alert("已连接")
+                };
+                websocket.onmessage = function (event) {
+                    alert(event.data);
+                };
+                websocket.onclose = function (event) {
+                    alert("已关闭");
+                };
+                //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+                window.onbeforeunload = function () {
+                    websocket.close();
+                }
+            }
+        });
+        $("#sendMessage").click(function () {
+            websocket.send($("#message").val());
+        });
+    });
+</script>
