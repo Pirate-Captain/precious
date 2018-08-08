175029917d79e1672a52e9af5544ced67e474c9b
Pirate-Captain <1531515382@qq.com>
2018-08-08 16:44:01 +0800

    parse pdf with the pdfbox

---
+++ precious/src/main/java/com/zyl/pdf/PdfboxTest.java
@@ -0,0 +1,33 @@
+/*
+ * chsi
+ * Created on 2018-07-17
+ */
+package com.zyl.pdf;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author zhuyl <a href="mailto:zhuyl@chsi.com.cn">zhu Youliang</a>
+ * @version $Id$
+ */
+public class PdfboxTest {
+    private static final String PDF_PATH = "d:/logs/pdf/16653911057.pdf";
+
+    public static void main(String[] args) throws IOException {
+        try {
+            PDDocument document = PDDocument.load(new File(PDF_PATH));
+            // 读文本内容
+            PDFTextStripper stripper = new PDFTextStripper();
+            // 设置按顺序输出
+            stripper.setSortByPosition(true);
+            //获取内容
+            System.out.println(stripper.getText(document));
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+    }
+}
