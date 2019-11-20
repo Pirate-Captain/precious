a94081c0930bb8b704cd49b28b10772851d0e5a5
Pirate-Captain <1531515382@qq.com>
2019-11-20 14:00:43 +0800

    red black tree

---
+++ precious/src/main/java/com/zyl/tree/rb/RedBlackTreeRotateRight.java
@@ -0,0 +1,33 @@
+/*
+ * chsi
+ * Created on 2019-11-07
+ */
+package com.zyl.tree.rb;
+
+/**
+ * 红黑树，右旋转
+ *
+ * @author zhuyl <a href="mailto:zhuyl@chsi.com.cn">zhu Youliang</a>
+ * @version $Id$
+ */
+public class RedBlackTreeRotateRight {
+    private static TreeNode<String, String> root = null;
+
+    public static void rotateRight(TreeNode<String, String> node) {
+        TreeNode<String, String> newParent = node.getLeft();
+        node.setLeft(newParent.getRight());
+        if ( null != newParent.getRight() ) {
+            newParent.getRight().setParent(node);
+        }
+        newParent.setParent(node.getParent());
+        if ( null == node.getParent() ) {
+            root = newParent;
+        } else if ( node.getParent().getLeft() == node ) {
+            node.getParent().setLeft(newParent);
+        } else if ( node.getParent().getRight() == node ) {
+            node.getParent().setRight(newParent);
+        }
+        node.setParent(newParent);
+        newParent.setRight(node);
+    }
+}
